0b89f656764477d2eaf5da78bb9504d4a58364d8
vocaltech <it@vocaltech.fr>
2024-05-17 19:44:46 +0200

    Add test for webhookAppController.createOrder()

--- apps/webhook-app/src/webhook-app.controller.spec.ts
+++ apps/webhook-app/src/webhook-app.controller.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WebhookAppController } from './webhook-app.controller';
 import { WebhookAppService } from './webhook-app.service';
+import { HttpModule } from '@nestjs/axios';
 
 describe('WebhookAppController', () => {
   let webhookAppController: WebhookAppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule],
       controllers: [WebhookAppController],
       providers: [WebhookAppService],
     }).compile();
@@ -15,8 +17,12 @@ describe('WebhookAppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(webhookAppController.getHello()).toBe('Hello World!');
+    it('should return data', () => {
+      const data = {
+        id: 1,
+        name: 'order_1',
+      };
+      expect(webhookAppController.createOrder(data)).toBe(data);
     });
   });
 });
