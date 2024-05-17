98c8842b68ec9a4a845b7dccd1e55cc3e32878f9
vocaltech <it@vocaltech.fr>
2024-05-17 19:46:12 +0200

    Remove getHello() from WebhookAppService

--- apps/webhook-app/src/webhook-app.service.ts
+++ apps/webhook-app/src/webhook-app.service.ts
@@ -2,10 +2,6 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class WebhookAppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-
   createOrder = (data: any): any => {
     return data;
   };
